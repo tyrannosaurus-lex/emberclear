4356fd3e06c96f03e8020095f6ccb18923fae911
NullVoxPopuli <LPSego3+dev@gmail.com>
2018-08-30 21:39:17 -0400

    still no progress

--- packages/frontend/src/ui/components/keyboard-press/component.ts
+++ packages/frontend/src/ui/components/keyboard-press/component.ts
@@ -1,12 +1,31 @@
 import Component from '@ember/component';
-import { EKMixin, keyDown, EKOnInsertMixin } from 'ember-keyboard';
+import { EKMixin, keyDown, EKOnInsertMixin, EKOnInitMixin, EKFirstResponderOnFocusMixin } from 'ember-keyboard';
 
-const KeyboardAwareComponent = Component.extend(EKMixin, EKOnInsertMixin);
+// const KeyboardAwareComponent = Component.extend(EKMixin, EKOnInsertMixin);
 
-export default class KeyboardPress extends KeyboardAwareComponent {
+export default Component.extend(EKMixin,
+  EKOnInitMixin,
+  EKOnInsertMixin,
+  EKFirstResponderOnFocusMixin, {
+    keyboardPriority: 100000000000000000000000000000000000000,
+
   didInsertElement() {
     this._super(...arguments);
 
-    this.on(keyDown(this.key), this.onPress);
+    this.on(keyDown(this.key), this.why);
+  },
+
+  why() {
+    console.log('sigh');
+
+    this.onPress();
   }
-}
+});
+
+// export default class KeyboardPress extends KeyboardAwareComponent {
+//   didInsertElement() {
+//     this._super(...arguments);
+
+//     this.on(keyDown(this.key), this.onPress);
+//   }
+// }
