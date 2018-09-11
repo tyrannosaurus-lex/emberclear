c27e8d0ec78add76fac9ddf72986f28cf0b17f5a
NullVoxPopuli <LPSego3+dev@gmail.com>
2018-09-11 16:08:39 -0400

    remove rsvp hash usage in one spot

--- packages/frontend/src/ui/routes/chat/route.ts
+++ packages/frontend/src/ui/routes/chat/route.ts
@@ -1,4 +1,3 @@
-import RSVP from 'rsvp';
 import Route from '@ember/routing/route';
 import { service } from '@ember-decorators/service';
 
@@ -32,7 +31,7 @@ export default class ChatRoute extends Route {
       include: 'sender'
     });
 
-    return RSVP.hash({ messages: records });
+    return { messages: records };
   }
 
   @disableInFastboot
