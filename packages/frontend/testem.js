25af2bf7210fc4d479978d7bfa1be7d86cebb214
NullVoxPopuli <LPSego3+dev@gmail.com>
2018-09-12 17:09:27 -0400

    eh?

--- packages/frontend/testem.js
+++ packages/frontend/testem.js
@@ -2,8 +2,8 @@ module.exports = {
   test_page: 'tests/index.html?hidepassed',
   disable_watching: true,
   launch_in_ci: [
-    // 'Chrome',
-    'Firefox'
+    'Chrome',
+    // 'Firefox'
   ],
   launch_in_dev: [
     'Chrome'
