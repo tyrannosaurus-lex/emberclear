ff008586e5a66d1cb4c7a4dc2553a8466436ab75
NullVoxPopuli <LPSego3+dev@gmail.com>
2018-11-12 20:36:25 -0500

    tests pass

--- packages/frontend/src/data/models/application/adapter.js
+++ packages/frontend/src/data/models/application/adapter.js
@@ -2,5 +2,31 @@ import LFAdapter from 'ember-localforage-adapter/adapters/localforage';
 
 export default LFAdapter.extend({
   namespace: 'emberclear',
-  caching: 'all'
+  caching: 'none',
+
+  shouldBackgroundReloadRecord() {
+    return true;
+  },
+
+  shouldBackgroundReloadAll() {
+    return true;
+  },
+
+  // ember-localforage does not throw valuable errors
+  queryRecord(/* store, type, query */) {
+    return new Promise((resolve, reject) => {
+      return this._super(...arguments)
+        .then(resolve)
+        .catch(() => reject('no record found in localforage'));
+    });
+  },
+
+  // ember-localforage does not throw valuable errors
+  findRecord(store, type, id) {
+    return new Promise((resolve, reject) => {
+      return this._super(...arguments)
+        .then(resolve)
+        .catch(() => reject(`record (${type}:${id}) not found in localforage`));
+    });
+  },
 });
