0674b8f6b9e04ad92ccc6e3d3c6a466687ea3c46
NullVoxPopuli <LPSego3+dev@gmail.com>
2018-11-25 08:03:07 -0500

    progress

--- packages/frontend/src/ui/components/search/component.ts
+++ packages/frontend/src/ui/components/search/component.ts
@@ -2,27 +2,40 @@ import Component, { tracked } from 'sparkles-component';
 import { service } from '@ember-decorators/service';
 import { keepLatestTask } from 'ember-concurrency-decorators';
 import { timeout } from 'ember-concurrency';
+import uuid from 'uuid';
 
 export default class SearchModal extends Component {
   @service store;
 
   searchText = '';
+  inputId = uuid();
 
   @tracked identityResults = [];
   @tracked channelResults = [];
+
+  didInsertElement() {
+    this.search.perform('');
+  }
+
+  didRender() {
+    document.getElementById(this.inputId).focus();
+  }
 
   submitSearch() {
     this.search.perform(this.searchText);
   }
 
   @keepLatestTask * search(searchTerm: string) {
-    console.log('eh', searchTerm);
     yield timeout(200);
 
     const identityResults = yield this.store.findAll('identity', { name: searchTerm });
     const channelResults = yield this.store.findAll('channel', { name: searchTerm });
 
-    this.identityResults = identityResults.filter(i => i.name.includes(searchTerm));
-    this.channelResults = channelResults.filter(i => i.name.includes(searchTerm));
+    this.identityResults = identityResults
+      .filter(i => i.name.includes(searchTerm) && i.id !== 'me')
+      .slice(0, 5);
+    this.channelResults = channelResults
+      .filter(i => i.name.includes(searchTerm))
+      .slice(0, 5);
   }
 }
