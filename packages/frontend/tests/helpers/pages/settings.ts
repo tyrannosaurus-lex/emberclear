3b81304c38ac4d20a2a90c765976b020042b740a
NullVoxPopuli <LPSego3+dev@gmail.com>
2018-12-31 12:54:27 -0500

    _woo_

--- packages/frontend/tests/helpers/pages/settings.ts
+++ packages/frontend/tests/helpers/pages/settings.ts
@@ -1,6 +1,23 @@
 import { click, find, fillIn } from '@ember/test-helpers';
+import { create, collection, clickable, isVisible, fillable } from 'ember-cli-page-object';
 
 const wrapper = '[data-test-settings-wrapper]';
+
+export const page = create({
+  relays: {
+    addRelay: clickable('[data-test-add-relay]'),
+    form: {
+      scope: '[data-test-add-relay-form]',
+      isVisible: isVisible(),
+      fillSocket: fillable('[data-test-socket-field]'),
+      fillOg: fillable('[data-test-og-field]'),
+      save: clickable('[data-test-save-relay]'),
+    },
+    table: {
+      rows: collection('[data-test-relays] tbody tr'),
+    },
+  },
+});
 
 export const settings = {
   save: () => click(`${wrapper} [data-test-save]`),
